6109f47ed41a803822dcf12d93f0d038b95f625d
Andre Insigne <cominteract@gmail.com>
2021-05-21 07:32:15 +0800

    schema change for reservations

--- src/schema.js
+++ src/schema.js
@@ -2,7 +2,7 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    reservations( ): ReservationConnection!
+    reservations : ReservationConnection!
     reservation(id: ID!): Reservation
   }
 
